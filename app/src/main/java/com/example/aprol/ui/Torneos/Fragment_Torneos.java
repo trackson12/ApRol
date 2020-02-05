7a8c7f610a81071139bbed9dd1e35395114a0421
afana <afana500@gmail.com>
2020-02-05 20:06:39 +0100

    Lista torneos

--- app/src/main/java/com/example/aprol/ui/Torneos/Fragment_Torneos.java
+++ app/src/main/java/com/example/aprol/ui/Torneos/Fragment_Torneos.java
@@ -1,12 +1,17 @@
 package com.example.aprol.ui.Torneos;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.speech.RecognitionService;
 import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,11 +20,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.aprol.R;
+import com.example.aprol.objeto.Juego;
+import com.example.aprol.objeto.Torneo;
+import com.example.aprol.rest.APIUtils;
+import com.example.aprol.rest.RestTorneo;
+import com.example.aprol.ui.Juegos.AdapterJuegos;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -27,8 +46,15 @@ public class Fragment_Torneos extends Fragment {
     private View root;
     ListView torneoView;
     ConstraintLayout constr;
+    FloatingActionButton voz;
     private TextView mEntradaTexto;
+    RestTorneo torneoRest;
     private static final int captura_voz=100;
+    ListView listView;
+    AdapterTorneos adapter;
+    List<Torneo> list = new ArrayList<Torneo>();
+    private FragmentManager fm;
+    private RecyclerView recyclerView;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -36,14 +62,78 @@ public class Fragment_Torneos extends Fragment {
         root= inflater.inflate(R.layout.fragment_lista_torneos, container, false);
         constr = (ConstraintLayout) root.findViewById(R.id.rvFragmentJuego);
         torneoView = (ListView) root.findViewById(R.id.tournament_list);
+        voz = (FloatingActionButton) root.findViewById(R.id.faButton);
 
 
-        iniciarEntradaVoz();
+
+
+        voz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarEntradaVoz();
+            }
+        });
+
+        if(isNetworkAvailable()) {
+            torneoRest =  APIUtils.getServiceTorneo();
+            listarProductos();
+
+        }else{
+            Toast.makeText(getContext(), "Es necesaria una conexión a internet", Toast.LENGTH_SHORT).show();
+        }
+
 
 
 
 
         return root;
+    }
+
+
+    private boolean isNetworkAvailable() {
+        /*
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) this.getSystemService
+                (Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+
+         */
+        ConnectivityManager connMgr = (ConnectivityManager) getActivity()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        return networkInfo != null && networkInfo.isConnected();
+    }
+    private void listarProductos() {
+
+        // Creamos la tarea que llamará al servicio rest y la encolamos
+        Call<List<Torneo>> call = torneoRest.findAll();
+        call.enqueue(new Callback<List<Torneo>>() {
+            @Override
+            public void onResponse(Call<List<Torneo>> call, Response<List<Torneo>> response) {
+                if(response.isSuccessful()){
+                    // Si tienes exito nos quedamos con el ResponseBody, listado en JSON
+                    // Nos hace el pasrser automáticamente
+                    list = response.body();
+                    // adapter = new AdapterJuegos(list, getContext(),getActivity(),getFragmentManager());
+
+                    recyclerView.setHasFixedSize(true);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getBaseContext()));
+                    recyclerView.setAdapter(new AdapterTorneos((ArrayList<Torneo>) list,getContext(),getFragmentManager()));
+
+                    //listView.setAdapter( new AdapterJuegos((ArrayList<Juego>) list,getContext()));
+                    //listView.setAdapter( new AdapterJuegos((ArrayList<Juego>) list,getContext()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Torneo>> call, Throwable t) {
+                Log.e("ERROR: ", t.getMessage());
+            }
+        });
+
     }
 
     private void iniciarEntradaVoz() {
